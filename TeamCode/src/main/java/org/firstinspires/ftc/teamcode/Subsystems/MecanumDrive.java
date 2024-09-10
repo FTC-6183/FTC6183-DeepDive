26d571271b94e5a29a5e434ed72445b95a91d117
ayaamghi <ayaamghi@gmail.com>
2024-09-09 21:34:04 -0400

    Command based with bulk caching/photon

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/MecanumDrive.java
@@ -37,6 +37,23 @@ public class MecanumDrive extends SubsystemBase {
         frontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
 
+    }
+
+    public void robotDrive(double y, double x, double rx) {
+        // Denominator is the largest motor power (absolute value) or 1
+        // This ensures all the powers maintain the same ratio,
+        // but only if at least one is out of the range [-1, 1]
+        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+        double frontLeftPower = (y + x + rx) / denominator;
+        double backLeftPower = (y - x + rx) / denominator;
+        double frontRightPower = (y + x - rx) / denominator;
+        double backRightPower = (y - x - rx) / denominator;
+
+        frontLeft.setPower(frontLeftPower);
+        backLeft.setPower(backLeftPower);
+        frontRight.setPower(frontRightPower);
+        backRight.setPower(backRightPower);
+
 
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/CMU.java
@@ -0,0 +1,88 @@
+package org.firstinspires.ftc.teamcode.TeleOp;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.arcrobotics.ftclib.command.CommandScheduler;
+import com.outoftheboxrobotics.photoncore.Photon;
+import com.qualcomm.hardware.lynx.LynxModule;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcontroller.external.samples.UtilityOctoQuadConfigMenu;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.Commands.ManualDriveCommand;
+import org.firstinspires.ftc.teamcode.Subsystems.MecanumDrive;
+
+import java.util.List;
+
+@Photon
+public class CMU extends LinearOpMode {
+    private MecanumDrive drive;
+
+    private GamepadEx controller1;
+
+    private ElapsedTime time;
+
+    Telemetry telemetry;
+
+
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        drive = new MecanumDrive(hardwareMap);
+
+        controller1 = new GamepadEx(gamepad1);
+
+        time = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
+
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+
+
+        drive.setDefaultCommand(new ManualDriveCommand(
+                drive,
+                controller1::getLeftY,
+                controller1::getRightX,
+                controller1::getLeftX
+        ));
+
+        List<LynxModule> allHubs = hardwareMap.getAll(LynxModule.class);
+
+        for (LynxModule hub : allHubs) {
+            hub.setBulkCachingMode(LynxModule.BulkCachingMode.MANUAL);
+        }
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        while (opModeIsActive()) {
+            for (LynxModule hub : allHubs) {
+                hub.clearBulkCache();
+            }
+
+
+            time.reset();
+
+            for (LynxModule hub : allHubs) {
+                    hub.clearBulkCache();
+            }
+
+            double loop = 1000/time.milliseconds();
+
+
+            CommandScheduler.getInstance().run();
+
+
+            telemetry.addData("Loop (hz)", loop);
+
+            telemetry.update();
+
+        }
+    }
+
+}
